8b9c52bfcac38491b31132da63873cf1bdc06a19
IvyMachogu <IvyMachogu>
2023-04-28 02:20:45 -0700

    update on environ.c

--- environ.c
+++ environ.c
@@ -1,94 +1,89 @@
-
 #include "shell.h"
 
 /**
- * _myenv - prints the current environment
- * @info: Structure containing potential arguments. Used to maintain
- *          constant function prototype.
+ * env - prints the current environment
+ * @inf: Structure containing potential arguments. Used to maintain
+ * constant function prototype.
  * Return: Always 0
  */
-int _myenv(info_t *info)
+int env(info1 *inf)
 {
-	print_list_str(info->env);
-	return (0);
+        print_list_str(inf->env);
+        return (0);
 }
 
 /**
- * _getenv - gets the value of an environ variable
- * @info: Structure containing potential arguments. Used to maintain
+ * getval - gets the value of an environ variable
+ * @inf: Structure containing potential arguments. Used to maintain
  * @name: env var name
- *
  * Return: the value
  */
-char *_getenv(info_t *info, const char *name)
+char *getval(info1 *inf, const char *name)
 {
-	list_t *node = info->env;
-	char *p;
+        list_t *node = inf->env;
+        char *h;
 
-	while (node)
-	{
-		p = starts_with(node->str, name);
-		if (p && *p)
-			return (p);
-		node = node->next;
-	}
-	return (NULL);
+        while (node)
+        {
+                h = starts_with(node->str, name);
+                if (h && *h)
+                        return (h);
+                node = node->next;
+        }
+        return (NULL);
 }
 
 /**
- * _mysetenv - Initialize a new environment variable,
+ * newenvvar - Initialize a new environment variable,
  *             or modify an existing one
- * @info: Structure containing potential arguments. Used to maintain
+ * @inf: Structure containing potential arguments. Used to maintain
  *        constant function prototype.
  *  Return: Always 0
  */
-int _mysetenv(info_t *info)
+int newenvvar(info1 *inf)
 {
-	if (info->argc != 3)
-	{
-		_eputs("Incorrect number of arguements\n");
-		return (1);
-	}
-	if (_setenv(info, info->argv[1], info->argv[2]))
-		return (0);
-	return (1);
+        if (inf->argc != 3)
+        {
+                _eputs("Incorrect number of arguements\n");
+                return (1);
+        }
+        if (setenv(inf, inf->argv[1], inf->argv[2]))
+                return (0);
+        return (1);
 }
 
 /**
- * _myunsetenv - Remove an environment variable
- * @info: Structure containing potential arguments. Used to maintain
- *        constant function prototype.
- * Return: Always 0
+ * _unsetenv - Remove an environment  variable
+ * @inf: Structure containing potential arguments. Used to maintain
+ * constant function prototype.
+ * return:Always 0
  */
-int _myunsetenv(info_t *info)
+int _unsetenv(info1 *inf)
 {
-	int i;
+        int a;
 
-	if (info->argc == 1)
-	{
-		_eputs("Too few arguements.\n");
-		return (1);
-	}
-	for (i = 1; i <= info->argc; i++)
-		_unsetenv(info, info->argv[i]);
-
-	return (0);
-}
-
+        if (inf->argc == 1)
+        {
+                _eputs("too few arguments.\n"
+                                return (1);
+                                }
+                                for (a = 1; a <= inf->argc; a++)
+                                unsetenv(inf, inf->argv[a];
+                                        return (0);
+                                        }
 /**
- * populate_env_list - populates env linked list
- * @info: Structure containing potential arguments. Used to maintain
- *          constant function prototype.
- * Return: Always 0
+ * popenvlist -populates env linked list
+ * @inf: Structure Containing potential arguments. used to maintain
+ * constant function prototype.
+ * Return: Always 0.
  */
-int populate_env_list(info_t *info)
+int popenvlist(info1 *inf)
 {
-	list_t *node = NULL;
-	size_t i;
+        list_t *node = NULL;
+        size_t a;
 
-	for (i = 0; environ[i]; i++)
-		add_node_end(&node, environ[i], 0);
-	info->env = node;
-	return (0);
+        for (a = 0; environ[a]; a++)
+                add_node_end(&node, environ[a], 0);
+        inf->env = node;
+        return (0);
 }
-
