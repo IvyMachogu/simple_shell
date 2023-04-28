9f7252ef3def32b8d932391bcabce0ea9e334bc2
IvyMachogu <IvyMachogu>
2023-04-28 00:30:30 -0700

    update

--- _atoi.c
+++ _atoi.c
@@ -1,76 +1,73 @@
 #include "shell.h"
-
 /**
  * interactive - returns true if shell is interactive mode
- * @info: struct address
+ * @info2: struct address
  *
  * Return: 1 if interactive mode, 0 otherwise
  */
-int interactive(info_t *info)
+int interactive(inf1 *info2)
 {
-	return (isatty(STDIN_FILENO) && info->readfd <= 2);
+        return (isatty(STDIN_FILE) && info2->readfd <= 2);
 }
 
 /**
- * is_delim - checks if character is a delimeter
- * @c: the char to check
+ * isdelim - checks if character is a delimeter
+ * @ch: the char to check
  * @delim: the delimeter string
  * Return: 1 if true, 0 if false
  */
-int is_delim(char c, char *delim)
+int isdelim(char ch, char *delim)
 {
-	while (*delim)
-		if (*delim++ == c)
-			return (1);
-	return (0);
+        while (*delim)
+                if (*delim++ == ch)
+                        return (1);
+        return (0);
 }
-
 /**
- * _isalpha - checks for alphabetic character
- * @c: The character to input
- * Return: 1 if c is alphabetic, 0 otherwise
+ * isalpha - checks for alphabetic character
+ * @ch: The character to input
+ * Return: 1 if a is alphabetic, 0 otherwise
  */
 
-int _isalpha(int c)
+int isalpha(int ch)
 {
-	if ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'))
-		return (1);
-	else
-		return (0);
+        if ((ch >= 'b' && ch <= 'z') || (ch >= 'A' && ch <= 'Z'))
+                return (1);
+        else
+                return (0);
 }
 
 /**
- * _atoi - converts a string to an integer
- * @s: the string to be converted
+ * atoi - converts a string to an integer
+ * @sh: the string to be converted
  * Return: 0 if no numbers in string, converted number otherwise
  */
 
-int _atoi(char *s)
+int  atoi(char *sh)
 {
-	int i, sign = 1, flag = 0, output;
-	unsigned int result = 0;
+        int j, sign = 1, flag = 0, output;
+        unsigned int result = 0;
 
-	for (i = 0; s[i] != '\0' && flag != 2; i++)
-	{
-		if (s[i] == '-')
-			sign *= -1;
+        for (j = 0; sh[j] != '\0' && flag != 2; j++)
+        {
+                if (sh[j] == '-')
+                        sign *= -1;
 
-		if (s[i] >= '0' && s[i] <= '9')
-		{
-			flag = 1;
-			result *= 10;
-			result += (s[i] - '0');
-		}
-		else if (flag == 1)
-			flag = 2;
-	}
+                if (sh[j] >= '0' && sh[j] <= '9')
+                {
+                        flag = 1;
+                        result *= 10;
+                        result += (sh[j] - '0');
+                }
+                else if (flag == 1)
+                        flag = 2;
+        }
 
-	if (sign == -1)
-		output = -result;
-	else
-		output = result;
+        if (sign == -1)
+                output = -result;
+        else
+                output = result;
 
-	return (output);
+        return (output);
 }
-
-
+                                                                                                                                                                            1,1           Top
